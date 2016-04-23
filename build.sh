9999b861d680469fe3e3410e3b0270d40630fee7
Adrien Pensart <adrien.pensart@corp.ovh.ca>
2016-04-23 19:32:37 -0400

    using gpg2 and temp file for env test

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t rsync-s3ql .
--- launch.sh
+++ launch.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -d --env-file prod.env --privileged -p 2222:22 -t rsync-s3ql
+tmpfile=$(mktemp /tmp/test-XXX.env)
+echo "Using $tmpfile"
+gpg2 --decrypt -r adrien.pensart@corp.ovh.ca testcontainer.env.gpg > $tmpfile
+docker run -d --env-file $tmpfile --privileged -p 2222:22 -t rsync-s3ql
